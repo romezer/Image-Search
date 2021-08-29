ebc328f4fd61da03f65227f1671dbc61714ec19d
romezer <romezer@gmail.com>
2021-08-30 00:14:15 +0300

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ import axios from 'axios';
 export default axios.create({
     baseUrl: 'https://api.unsplash.com',
     headers:{
-        Authorization: 'Client-ID 0fcdbbe49eff3b855d044952981b327e58f47625459110b6013a216701efbe30'
+        Authorization: 'Client-ID'
     }
 });
